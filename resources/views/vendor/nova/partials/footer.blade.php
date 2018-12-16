374ade206eb50b36f5eff970706fc57b9295067b
Joe Elia <mrjoeelia@gmail.com>
2018-12-16 16:51:26 -0500

    WIP

--- resources/views/vendor/nova/partials/footer.blade.php
+++ resources/views/vendor/nova/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <p class="mt-8 text-center text-xs text-80">
-    <a href="http://nova.laravel.com" class="text-primary dim no-underline">Laravel Nova</a>
+    <a href="http://openounce.com" class="text-primary dim no-underline">OpenOunce Technologies</a>
     <span class="px-1">&middot;</span>
-    &copy; {{ date('Y') }} Laravel LLC - By Taylor Otwell, David Hemphill, and Steve Schoger.
+    &copy; {{ date('Y') }} OpenOunce Technologies LLC.
     <span class="px-1">&middot;</span>
     v{{ Laravel\Nova\Nova::version() }}
 </p>
